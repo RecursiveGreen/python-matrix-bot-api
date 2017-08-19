c46663db469d8d0349b231d426d4a04697393c22
Josh Washburne <recursive.green@gmail.com>
2017-08-19 01:02:42 -0400

    Forgot emoji import. Added some more to response.

--- matrix_bot_api/handlers/dance.py
+++ matrix_bot_api/handlers/dance.py
@@ -1,3 +1,5 @@
+from emoji import emojize
+
 from matrix_bot_api.mcommand_handler import MCommandHandler
 from matrix_bot_api.media import prepare_image
 
@@ -11,8 +13,9 @@ def dance_callback(room, event):
         pic = prepare_image(room.client, DANCE)
         room.send_image(pic['uri'], pic['filename'], **pic['info'])
     else:
-        resp = emoji.emojize("shakes his money-maker! :notes: :dancers: :notes:",
-                             use_aliases=True)
+        resp = emojize(("shakes his money-maker! "
+                        ":dancers: :notes: :dancers: :notes: :dancers:"),
+                        use_aliases=True)
         room.send_emote(resp)
 
 dance_handler = MCommandHandler("dance", dance_callback)
