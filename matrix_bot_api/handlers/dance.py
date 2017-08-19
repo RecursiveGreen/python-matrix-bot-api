ede849d3c5d65f986e0d1e52aec8da9e2a9a9389
Josh Washburne <recursive.green@gmail.com>
2017-08-19 00:54:17 -0400

    Catch uninitialized dance settings.

--- matrix_bot_api/handlers/dance.py
+++ matrix_bot_api/handlers/dance.py
@@ -1,10 +1,18 @@
 from matrix_bot_api.mcommand_handler import MCommandHandler
 from matrix_bot_api.media import prepare_image
 
-from config import DANCE
+try:
+    from config import DANCE
+except ImportError:
+    DANCE = None
 
 def dance_callback(room, event):
-    pic = prepare_image(room.client, DANCE)
-    room.send_image(pic['uri'], pic['filename'], **pic['info'])
+    if DANCE:
+        pic = prepare_image(room.client, DANCE)
+        room.send_image(pic['uri'], pic['filename'], **pic['info'])
+    else:
+        resp = emoji.emojize("shakes his money-maker! :notes: :dancers: :notes:",
+                             use_aliases=True)
+        room.send_emote(resp)
 
 dance_handler = MCommandHandler("dance", dance_callback)
